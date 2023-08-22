438ec48975ba30dc5bd14b82718ee4a37e662acd
Ashutosh Sharma <ashutoshsharmadba@gmail.com>
2023-08-22 23:58:20 +0530

    22 Aug

--- hello_world/lib/dice_roller.dart
+++ hello_world/lib/dice_roller.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
-class DiceRoller extends StatefulWidget {//we will not add build because its a stateful widget
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  //we will not add build because its a stateful widget
   const DiceRoller({Key? key}) : super(key: key);
   @override
-  State<DiceRoller> createState(){
+  State<DiceRoller> createState() {
     return _DiceRollerState();
-  }  
+  }
 }
+
 class _DiceRollerState extends State<DiceRoller> {
-    var activeDiceImage = 'assets/images/dice-2.png';
-    void rollDice() {
+  var currentDiceRoll = 2;
+
+  void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/images/dice-1.png';
-    });        
+      currentDiceRoll = Random().nextInt(6) + 1;
+    });
   }
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Column(
-          mainAxisSize: MainAxisSize
-              .min, //Column is the parent widget, mainAxisSize is the property of the Column widget
-          //mainAxisSize.min means that the Column widget will take up the minimum amount of space in the main axis
-          children: [
-            Image.asset(activeDiceImage, width: 200, height: 200), //Image Asset
-            const SizedBox(height: 20), //SizedBox, alternative of padding
-            TextButton(
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                    // padding: const EdgeInsets.only(
-                    //   top: 20,),// we can use all also instead of only, all is used to set the same value for all the sides
-                    foregroundColor: Color.fromARGB(255, 5, 0, 248),
-                    textStyle: const TextStyle(fontSize: 30)),
-                child: const Text('Roll Dice'))
-          ],
-        );
+      mainAxisSize: MainAxisSize
+          .min, //Column is the parent widget, mainAxisSize is the property of the Column widget
+      //mainAxisSize.min means that the Column widget will take up the minimum amount of space in the main axis
+      children: [
+        Image.asset('assets/images/dice-$currentDiceRoll.png',
+            width: 200, height: 200), //Image Asset
+        const SizedBox(height: 20), //SizedBox, alternative of padding
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,),// we can use all also instead of only, all is used to set the same value for all the sides
+                foregroundColor: Color.fromARGB(255, 5, 0, 248),
+                textStyle: const TextStyle(fontSize: 30)),
+            child: const Text('Roll Dice'))
+      ],
+    );
   }
 }
